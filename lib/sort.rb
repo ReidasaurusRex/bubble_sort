c232ce5d812f13a776e3f0cd9d1f4415aec03203
ReidasaurusRex <reidhlewis91@gmail.com>
2015-03-16 16:48:40 -0700

    Ruby solution with optimization for average and best cases. I think.

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,6 +1,27 @@
 class Array
 
   def bubble_sort
+    i = 0
+     current_length = self.length
+    while i < self.length do 
+        swap_counter = 0
+        j = 0
+        k = 1
+        while j < current_length-1 do 
+            if self[j] > self[k]
+                self[j], self[k] = self[k], self[j]
+                swap_counter += 1
+            end
+            j += 1
+            k += 1
+        end
+        current_length -= 1
+        if swap_counter == 0
+            return self
+        end
+        i += 1
+    end
+    return self
   end
 
 end

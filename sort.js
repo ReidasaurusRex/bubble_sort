50340d52d2fa109e5c84db12c734629a1a2d0a2c
ReidasaurusRex <reidhlewis91@gmail.com>
2015-03-16 18:02:10 -0700

    Finalized JS solution with optimization

--- sort.js
+++ sort.js
@@ -1,7 +1,28 @@
 function bubbleSort(arr){
-
+  var i = 0;
+  var currentLength = arr.length;
+  for (i; i < arr.length; i++) {
+    var swapCounter = 0;
+    var j = 0;
+    var k = 1;
+    for (j; j < currentLength - 1; j++) {
+      var jelement = arr[j];
+      if (arr[j] > arr[k]) {
+        arr[j] = arr[k];
+        arr[k] = jelement;
+        swapCounter++;
+      }
+      k++;
+      }
+      currentLength--;
+      if (swapCounter === 0) {
+        return arr;
+    }
+  i++;
+  }
+  return arr;
 }
 
 // do not touch this
-module.exports = bubbleSort
+module.exports = bubbleSort;
 
